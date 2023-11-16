b9ca668bc2a670af7237d16fb0558ad5ae51ff0c
Ray <raylamas@att.net>
2023-11-16 00:55:39 -0600

    fghfgh

--- pybank/main.py
+++ pybank/main.py
@@ -17,19 +17,18 @@ with open(budget_csv, 'r') as budgetfile:
     for row in budget_reader:
         budget_data[row[0]] = int(row[1])
 #Calculate the changes in profit/loss over the whole period
-    profit_loss_changes = []
-    for i in range(1, len(budget_data)):
-    change = budget_data[i] - budget_data[i-1]
+profit_loss_changes = []
+values = list(budget_data.values())
+for i in range(1, len(budget_data)):
+    change = values[i] - values[i-1]
     profit_loss_changes.append(change)
-    print(profit_loss_changes)
+#Calculate the aveage change in profit/loss
+average_profit_loss_change = sum(profit_loss_changes) / len(profit_loss_changes)
 
 #total number of months
 total_months = len(budget_data)
 #total amount of profit/losses over the entire period
 total_profit_loss = sum(budget_data.values())
-print(total_profit_loss)
 
-
-
-average_profit_loss_change = []
-greatest_increase = {"date"}
+greatest_increase = max(profit_loss_changes)
+print(greatest_increase)
