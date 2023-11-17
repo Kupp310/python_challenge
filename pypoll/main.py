68e5113f6ef3d0ab885b73faa0fe3d07e597217d
Ray <raylamas@att.net>
2023-11-16 22:54:05 -0600

    Pypoll analysis complete

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,38 +1,42 @@
 import os
 import csv
 
-pypoll_csv = os.path.join("Resources","election_data.csv")
+election_data = {}
 
-with open(pypoll_csv) as csvfile:
+election_csv = os.path.join("Resources","election_data.csv")
+
+with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-    columns_as_lists = [list(c) for c in zip(csv_reader)]
-
-# Calculate total votes
-total_votes = len(pypoll_csv)
- 
+    next(csv_reader)
 # Get a list of unique candidates
-candidates = pypoll_csv["Candidate"].unique()
- 
-# Create a dictionary to store candidate votes
-candidate_votes = {}
- 
-# Loop through the data to count votes for each candidate
-for candidate in candidates:
-    candidate_votes[candidate] = pypoll_csv[pypoll_csv["Candidate"] == candidate]["Candidate"].count()
- 
-# Find the winner based on the popular vote
-winner = max(candidate_votes, key=candidate_votes.get)
- 
-# Print the results
-print("Election Results")
-print("-" * 28)
-print(f"Total Votes: {total_votes}")
-print("-" * 28)
- 
-for candidate in candidates:
-    percentage = (candidate_votes[candidate] / total_votes) * 100
-    print(f"{candidate}: {percentage:.3f}% ({candidate_votes[candidate]})")
- 
-print("-" * 28)
-print(f"Winner: {winner}")
-print("-" * 28)
+    unique_candidates = [row[2] for row in csv_reader]
+# Calculate total votes
+total_votes = len(unique_candidates)
+
+# List of candidates who received votes
+candidates_received_votes = list(set(unique_candidates))
+
+#Dictionary of Total votes won by each candidate
+votes_won_by_candidate = {candidate: unique_candidates.count(candidate) for candidate in candidates_received_votes}
+
+#Calculate percentage of votes won by each candidate in a dictionary
+vote_percentage = {candidate: (votes / total_votes) * 100 for candidate, votes in votes_won_by_candidate.items()}
+
+#Get the winner based on the popular vote
+election_winner = max(votes_won_by_candidate, key=votes_won_by_candidate.get)
+
+#write results to new file
+#new file path
+output_path = os.path.join("analysis", "election_winner.txt")
+
+#write the results in the new file
+with open(output_path, "w") as txtfile:
+    csvwriter = csv.writer(txtfile)
+    txtfile.write("Election Results\n")
+    txtfile.write("--------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write("---------------------------\n")
+    for candidate in candidates_received_votes:
+        txtfile.write(f"{candidate}: {vote_percentage[candidate]:.3f}% ({votes_won_by_candidate[candidate]})\n")
+    txtfile.write("---------------------------\n")
+    txtfile.write(f"Winner: {election_winner}")
